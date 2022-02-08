5ffa283ad5683da39d02193c281ac8b9b3c25556
samad516 <abdulsamadborn03@gmail.com>
2022-02-08 21:06:27 +0500

    updated

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/home', (req, res) => {
+    res.send('Home')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('Profile')
+})
+
+app.get('/about', (req, res) => {
+    res.send('about')
+})
+
+app.listen(port, () => {
+    console.log(`server app listening on port ${port}`)
+})
